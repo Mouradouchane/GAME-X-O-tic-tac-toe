a8d9c8cf7eb4e34809531247e94e177b91aac225
Mourad ouchane <mouradouchane@gmail.com>
2021-12-17 14:19:39 +0100

    game blocks with saved colors

--- game/javascript/table/load_table.js
+++ game/javascript/table/load_table.js
@@ -8,18 +8,35 @@ export class table{
         this.blocks = [];
 
         this.table_dom = document.querySelector("#GAME_TABLE");
-
         
+        // function must be run when user click on GO button
+        // this function it's task only load game table with right style
         this.load_table_elements = () => {
             // setup table grid
             this.table_dom.style.cssText = `grid-template-columns : repeat(${this.game_table_size}, 1fr)`;
             
-            //debugger
+            // loading "game_details_obj" for using saved values from user "colors , background ..."
+            let game_details_obj = JSON.parse( localStorage.getItem("game_details_obj") ); 
+            
+            // in case "game_details_obj" missing 
+            if(game_details_obj == null || game_details_obj == undefined){
+                console.error("GAME : missing 'game_details_obj' , go to sitting & make some changes");
+            }
+
+            let color1 = game_details_obj.sqr1_color;
+            let color2 = game_details_obj.sqr2_color;
+
+            // generate blocks
             for(let x = 1 ; x <= this.game_table_size*this.game_table_size ; x += 1){
+                
+                // a new block
                 let element = document.createElement("div");
+                // setup that new block
                     element.setAttribute("class","BLOCK_STYLE");
                     element.setAttribute("id","block"+x);
-
+                    element.style.cssText = `background-color : ${(x % 2 != 0) ? color1 : color2}`;
+                
+                // add block to the game table
                 this.table_dom.appendChild(element);  
                 this.blocks.push(element);
             }
